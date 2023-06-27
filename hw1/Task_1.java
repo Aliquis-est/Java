6f72b8c3607e7044aaaf0f81d3b86fbebd125a58
Anna <morozova.anna.2014year@gmail.com>
2023-06-27 21:47:34 +0300

    done

--- hw1/Task_1.java
+++ hw1/Task_1.java
@@ -1,6 +1,6 @@
 //  Задание1. Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 
-package HomeWork1;
+package hw1;
 
 import java.util.Scanner;
 
--- hw1/Task_2.java
+++ hw1/Task_2.java
@@ -1,5 +1,31 @@
-package HomeWork1;
+// 2) Вывести все простые числа от 1 до 1000
+
+package hw1;
 
 public class Task_2 {
-    
+
+
+    public static void main(String[] args) {
+        
+        int i, j;
+        boolean result;
+
+        System.out.println("Простые числа от 1 до 1000:");
+
+        for (i = 2; i <= 1000; i++) {
+            result = true;
+
+            for (j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    result = false;
+                    break;
+                }
+            }
+
+            if (result) {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
